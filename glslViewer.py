dab2fba369ff3154986a69652c66314fe6c8fb0a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-04-24 15:33:39 -0400

    adding command to reload

--- glslViewer.py
+++ glslViewer.py
@@ -1,10 +1,18 @@
 # Author @patriciogv - 2015
 # http://patriciogonzalezvivo.com
+
 import sublime, sublime_plugin, os, subprocess
 
-class OpenshaderCommand(sublime_plugin.EventListener):
+class GlslViewerCommand(sublime_plugin.EventListener):
     def on_load(self,view):
         f = view.file_name()
         if f.endswith('.frag') or f.endswith('.fs'):
             arg = ['glslViewer', view.file_name()]
+            subprocess.Popen(arg)
+
+class openshaderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        f = self.window.active_view().file_name()
+        if f.endswith('.frag') or f.endswith('.fs'):
+            arg = ['glslViewer', f]
             subprocess.Popen(arg)
