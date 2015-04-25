4eef42ea830b1dbe5eb10f54509f38a209391038
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-04-25 10:41:57 -0400

    adding creation of template

--- glslViewer.py
+++ glslViewer.py
@@ -19,4 +19,32 @@ class GlslViewerCommand(sublime_plugin.EventListener):
 class openshaderCommand(sublime_plugin.WindowCommand):
     def run(self):
         openShader(self.window.active_view().file_name())
+
+class glsltemplateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "\
+#ifdef GL_ES\n\
+precision mediump float;\n\
+#endif\n\
+\n\
+uniform float u_time;\n\
+uniform vec2 u_mouse;\n\
+uniform vec2 u_resolution;\n\
+\n\
+void main (void) {\n\
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;\n\
+    vec3 color = vec3(st,0.0);\n\
+\n\
+\n\
+    gl_FragColor = vec4(color,1.0);\n\
+}\n")
+
+class newshaderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        newView = self.window.new_file()
+        newView.run_command('glsltemplate')
+
+
+
+
         
