3d4d106e2b8711a4710060b7cde32d775ab385df
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-04-28 09:17:04 -0400

    running from system

--- glslViewer.py
+++ glslViewer.py
@@ -4,39 +4,27 @@
 import sublime, sublime_plugin, os, subprocess, sys
 
 version = "0.2.0"
-process = {}
 
-def openShader(view, shaderFile):
+def openShader(view):
+    shaderFile = view.file_name()
     if shaderFile.endswith('.frag') or shaderFile.endswith('.fs'):
         settings = sublime.load_settings('glslViewer.sublime-settings')
         path = settings.get('path')
-        # view = sublime.active_window().active_view()
-        # texturesLines = view.find_all('uniform sampler2D ')
-        # if len(texturesLines) > 0:
-        #     sublime.message_dialog('This shader use ' + str(len(texturesLines)) + ' textures. So far this textures can not be loaded, we are working to support this in future versions. Thank you.')
-        cmd = [path+'glslViewer', shaderFile]
-        process[shaderFile] = subprocess.Popen(cmd) #, stdout=subprocess.PIPE)
-        # view.run_command(' build "C:\Program Files\SomeApp\SomeApp.exe" "$File"')
+        texturesLines = view.find_all('sampler2D')
+        if len(texturesLines) > 0:
+            sublime.message_dialog('This shader use ' + str(len(texturesLines)) + ' textures. So far this textures can not be loaded, we are working to support this in future versions. Thank you.')
+            # for line in texturesLines:
+            #     sublime.message_dialog(line(line))
+        sublime.active_window().run_command('exec',{'cmd':[path+'glslViewer', shaderFile]})
 
 
 class GlslViewerCommand(sublime_plugin.EventListener):
     def on_load(self,view):
-        openShader(view,view.file_name())
-    # def on_post_save(self,view):
-    #     shaderFile = view.file_name()
-    #     if shaderFile in process:
-    #         if process[shaderFile] is not None:
-    #             if process[shaderFile].stdout is not None:
-    #                 print(process[shaderFile].stdout.readline())
-    def on_close(self,view):
-        shaderFile = view.file_name()
-        if shaderFile in process:
-            process[shaderFile].kill()
-            del process[shaderFile]
+        openShader(view)
 
 class openshaderCommand(sublime_plugin.WindowCommand):
     def run(self):
-        openShader(self.window.active_view(),self.window.active_view().file_name())
+        openShader(self.window.active_view())
 
 class glsltemplateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
