f8d3d4c54410918bc7d7f1f66efb541590788fde
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-04-28 07:42:50 -0400

    trying to lunch from build system

--- glslViewer.py
+++ glslViewer.py
@@ -6,7 +6,7 @@ import sublime, sublime_plugin, os, subprocess, sys
 version = "0.2.0"
 process = {}
 
-def openShader(shaderFile):
+def openShader(view, shaderFile):
     if shaderFile.endswith('.frag') or shaderFile.endswith('.fs'):
         settings = sublime.load_settings('glslViewer.sublime-settings')
         path = settings.get('path')
@@ -16,18 +16,18 @@ def openShader(shaderFile):
         #     sublime.message_dialog('This shader use ' + str(len(texturesLines)) + ' textures. So far this textures can not be loaded, we are working to support this in future versions. Thank you.')
         cmd = [path+'glslViewer', shaderFile]
         process[shaderFile] = subprocess.Popen(cmd) #, stdout=subprocess.PIPE)
+        # view.run_command(' build "C:\Program Files\SomeApp\SomeApp.exe" "$File"')
 
 
 class GlslViewerCommand(sublime_plugin.EventListener):
     def on_load(self,view):
-        openShader(view.file_name())
-    def on_post_save(self,view):
-        shaderFile = view.file_name()
-        if shaderFile in process:
-            if process[shaderFile] is not None:
-                if process[shaderFile].stdout is not None:
-                    print(process[shaderFile].stdout.readline())
-
+        openShader(view,view.file_name())
+    # def on_post_save(self,view):
+    #     shaderFile = view.file_name()
+    #     if shaderFile in process:
+    #         if process[shaderFile] is not None:
+    #             if process[shaderFile].stdout is not None:
+    #                 print(process[shaderFile].stdout.readline())
     def on_close(self,view):
         shaderFile = view.file_name()
         if shaderFile in process:
@@ -36,7 +36,7 @@ class GlslViewerCommand(sublime_plugin.EventListener):
 
 class openshaderCommand(sublime_plugin.WindowCommand):
     def run(self):
-        openShader(self.window.active_view().file_name())
+        openShader(self.window.active_view(),self.window.active_view().file_name())
 
 class glsltemplateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
